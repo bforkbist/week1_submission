4944e0f848bd21490b93c1155e16e4a02e61b271
bforkbist <59921267+bforkbist@users.noreply.github.com>
2020-02-25 00:06:48 +0530

    Update design.java

--- week1_code/week4_code/design.java
+++ week1_code/week4_code/design.java
@@ -4,20 +4,44 @@ class Pattern{
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int r=in.nextInt(),c=in.nextInt();
+        int count =0,count1=0;
         for(int i=0;i<r/2;i++){
-            for(int j=0;j<(c/2)-1;j++)
+            for(int j=1;j<(c/2)-(3*i);j++)
                 System.out.print("-");
+            for(int k=0;k<count;k++){
+            	System.out.print(".|.");
+            }
+            count++;
             System.out.print(".|.");
-            for(int j=0;j<c/2;j++)
+            for(int k=0;k<count1;k++){
+            	System.out.print(".|.");
+            }
+            count1++;
+            for(int j=1;j<(c/2)-(3*i);j++)
                 System.out.print("-");
             System.out.println("");
         }
-        System.out.println("welcome");
-        for(int i=0;i<r/2;i++){
-            for(int j=0;j<(c/2)-1;j++)
+        int t=(c-7)/2;
+        for(int i=0;i<t;i++)
+        	System.out.print("-");
+        System.out.print("WELCOME");
+        for(int i=0;i<t;i++)
+        	System.out.print("-");
+        System.out.println("");
+
+        for(int i=0;i<(r/2);i++){
+            for(int j=3*(i+1);j>0;j--)
                 System.out.print("-");
+            for(int k=count-1;k>0;k--){
+            	System.out.print(".|.");
+            }
+            count--;
             System.out.print(".|.");
-            for(int j=0;j<c/2;j++)
+            for(int k=count1-1;k>0;k--){
+            	System.out.print(".|.");
+            }
+            count1--;
+            for(int j=3*(i+1);j>0;j--)
                 System.out.print("-");
             System.out.println("");
         }
