679b9688cd925ca2db13da6844f5ba5d5e890bef
bforkbist <59921267+bforkbist@users.noreply.github.com>
2020-02-24 23:27:57 +0530

    Update design.java

--- week1_code/week4_code/design.java
+++ week1_code/week4_code/design.java
@@ -1,10 +1,26 @@
-import java.lang.reflect.Array;
 import java.util.Scanner;
 
-class Design{
+class Pattern{
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int i=in.nextInt(),j=in.nextInt();
-        
+        int r=in.nextInt(),c=in.nextInt();
+        for(int i=0;i<r/2;i++){
+            for(int j=0;j<(c/2)-1;j++)
+                System.out.print("-");
+            System.out.print(".|.");
+            for(int j=0;j<c/2;j++)
+                System.out.print("-");
+            System.out.println("");
+        }
+        System.out.println("welcome");
+        for(int i=0;i<r/2;i++){
+            for(int j=0;j<(c/2)-1;j++)
+                System.out.print("-");
+            System.out.print(".|.");
+            for(int j=0;j<c/2;j++)
+                System.out.print("-");
+            System.out.println("");
+        }
+        in.close();
     }
 }
